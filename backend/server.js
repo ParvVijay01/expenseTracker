6baaf6d3659d37cd195edcbcb6c7e374cfe91825
ParvVijay01 <parvvj2000@gmail.com>
2025-06-14 14:32:52 +0530

    backend initialised

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+
+const port = process.env.PORT || 5001
+
+app.get("/", (req, res) => {
+    res.send("It's working")
+})
+
+app.listen(port, () => {
+    console.log(`Server is up and running on port: ${port}`);
+})
