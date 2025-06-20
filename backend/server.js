84cd616be144eef32fe97ece0cb384696856e14a
ParvVijay01 <parvvj2000@gmail.com>
2025-06-20 20:47:29 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 import express from 'express'
 import dotenv from "dotenv"
-import { sql } from "./config/db.js"
-import rateLimiter from './middleware/rateLimiter.js'
-import transactionsRoute from "./routes/transactionsRoutes.js"
-
+import { initDB } from "./src/config/db.js"
+import rateLimiter from './src/middleware/rateLimiter.js'
+import transactionsRoute from "./src/routes/transactionsRoutes.js"
 
 dotenv.config()
 
@@ -14,29 +13,6 @@ const port = process.env.PORT || 5001
 //middleware
 app.use(express.json())
 app.use(rateLimiter)
-
-async function initDB() {
-    try {
-        await sql`CREATE TABLE IF NOT EXISTS transactions(
-        id SERIAL PRIMARY KEY,
-        user_id VARCHAR(255) NOT NULL,
-        title VARCHAR(255) NOT NULL,
-        amount DECIMAL(10,2) NOT NULL,
-        category VARCHAR(255) NOT NULL,
-        created_at DATE NOT NULL DEFAULT CURRENT_DATE
-        )`
-
-        console.log("DB initialised successfullly");
-        
-    } catch (error) {
-        console.log("Error initialising the DB", error);
-        process.exit(1) // status code 1 means failure, 0 success
-    }
-}
-
-app.get("/", (req,res) => {
-    res.send("Hey")
-})
 
 app.use("/api/transactions", transactionsRoute)
 
