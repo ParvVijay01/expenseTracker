8b4a9f9fd62f754d1a09c4db69132e09d478fbf0
ParvVijay01 <parvvj2000@gmail.com>
2025-06-29 16:38:22 +0530

    created basic layout of the app

---
+++ mobile/app/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { Text, View, Image, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // justifyContent: "center",
+    // alignItems: "center",
+    backgroundColor: "red"
+  }
+})
